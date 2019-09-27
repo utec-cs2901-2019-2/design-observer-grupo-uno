d030f1ce421f9a91cdcb2d7d7b39810eb3ff2346
Andre <andre.segovia@utec.edu.pe>
2019-09-27 12:40:53 -0500

    Continued development...

--- observer/src/com/company/App_movil.java
+++ observer/src/com/company/App_movil.java
@@ -2,6 +2,10 @@ package company;
 
 public class App_movil extends Observer {
   private Sensor sensor;
+  public App_movil(Sensor _sensor)
+  {
+    sensor = _sensor;
+  }
   public void update()
   {
     System.printl("La nueva temperatura es: " + sensor.getTemp());
--- observer/src/com/company/App_web.java
+++ observer/src/com/company/App_web.java
@@ -2,6 +2,10 @@ package company;
 
 public class App_web extends Observer {
   private Sensor sensor;
+  public App_web(Sensor _sensor)
+  {
+    sensor = _sensor;
+  }
   public void update()
   {
     System.printl("La nueva temperatura es: " + sensor.getTemp());
